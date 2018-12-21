b5b709c75795ad328cdefec3651df28ac0f24c6f
anujms1999 <anujshah242@gmail.com>
2018-12-21 17:02:00 +0530

    Coordinate retrieval done

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-#Cli app for predicting weather made using nodejs
+const request = require('request');
+const yargs = require('yargs');
+
+const argv  =yargs
+    .options({
+      a: {
+        demand: true,
+        alias : 'address',
+        describe: 'Address to fetch weather for',
+        string: true
+
+      }
+    })
+    .help()
+    .argv;
+
+var address = argv.address;
+var encodedAddress = encodeURIComponent(address);
+
+request({
+  url :'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress,
+  json: true
+},(error,response,body) =>{
+  console.log(`Address: ${ body.results[0].formatted_address}`);
+  console.log(`Latitude: ${ body.results[0].geometry.location.lat}`);
+  console.log(`Longitude: ${ body.results[0].geometry.location.lng}`);
+})
