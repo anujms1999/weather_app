71a76592ef32b5b7db6324511dbbe07ef81e8c85
anujms1999 <anujshah242@gmail.com>
2018-12-22 12:12:39 +0530

    Abstracting code

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const request = require('request');
 const yargs = require('yargs');
 
+const geocode = require('./geocode/geocode');
+
 const argv  =yargs
     .options({
       a: {
@@ -14,14 +16,5 @@ const argv  =yargs
     .help()
     .argv;
 
-var address = argv.address;
-var encodedAddress = encodeURIComponent(address);
+geocode.geocodeAddress(argv.address);
 
-request({
-  url :'https://maps.googleapis.com/maps/api/geocode/json?address=' + encodedAddress,
-  json: true
-},(error,response,body) =>{
-  console.log(`Address: ${ body.results[0].formatted_address}`);
-  console.log(`Latitude: ${ body.results[0].geometry.location.lat}`);
-  console.log(`Longitude: ${ body.results[0].geometry.location.lng}`);
-})
